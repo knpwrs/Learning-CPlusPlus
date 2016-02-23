ab14c91f10647d5a60798cd2c199f924b0a87e8f
Kenneth Powers <ken@kenpowers.net>
2016-02-23 17:28:48 -0500

    Add numeric string conversion examples.
    From Chapter 2 of Professional C++ Third Edition.

--- 01-professional-cplusplus/02-working-with-strings/test/01-strings-test.cpp
+++ 01-professional-cplusplus/02-working-with-strings/test/01-strings-test.cpp
@@ -7,10 +7,33 @@ using namespace PcppC02E01Strings;
 
 TEST(Strings, Copy) {
   string str = "FooBar";
-  EXPECT_TRUE(copy(str) == str);
+  EXPECT_EQ(copy(str), str);
 }
 
 TEST(Strings, CopyLiteral) {
   auto str = "FooBar"s;
-  EXPECT_TRUE(copy(str) == str);
+  EXPECT_EQ(copy(str), str);
+}
+
+TEST(Strings, FromNumber) {
+  EXPECT_EQ(to_string(-4), "-4"s);
+  EXPECT_EQ(to_string(4U), "4"s);
+  EXPECT_EQ(to_string(-5L), "-5"s);
+  EXPECT_EQ(to_string(5UL), "5"s);
+  EXPECT_EQ(to_string(-6LL), "-6"s);
+  EXPECT_EQ(to_string(6ULL), "6"s);
+  EXPECT_EQ(to_string(3.14f), "3.140000"s);
+  EXPECT_EQ(to_string(3.14), "3.140000"s);
+  EXPECT_EQ(to_string(3.14L), "3.140000"s);
+}
+
+TEST(Strings, ToNumber) {
+  EXPECT_EQ(stoi("-4"s), -4);
+  EXPECT_EQ(stol("-5"s), -5L);
+  EXPECT_EQ(stoul("5"s), 5L);
+  EXPECT_EQ(stoll("-7"s), -7LL);
+  EXPECT_EQ(stoull("7"s), 7LL);
+  EXPECT_EQ(stof("-3.14"s), -3.14f);
+  EXPECT_EQ(stod("3.14159"s), 3.14159);
+  EXPECT_EQ(stold("3.14159"s), 3.14159L);
 }
