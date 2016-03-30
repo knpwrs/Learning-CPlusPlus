2f303dd38b1ce75f7b544b55b5ea9ccc438bc8c3
Kenneth Powers <ken@kenpowers.net>
2016-03-29 22:59:12 -0400

    Extended Spreadsheet tests.

--- 01-professional-cplusplus/07-08-classes-and-objects/test/SpreadSheetTest.cpp
+++ 01-professional-cplusplus/07-08-classes-and-objects/test/SpreadSheetTest.cpp
@@ -10,7 +10,7 @@ TEST(Spreadsheet, Statics)
   EXPECT_EQ(Spreadsheet::kMaxHeight, 100);
 }
 
-TEST(Spreadsheet, Constructor)
+TEST(Spreadsheet, BasicOps)
 {
   Spreadsheet ss(32, 32);
   EXPECT_EQ(ss.getId(), 0);
@@ -19,4 +19,11 @@ TEST(Spreadsheet, Constructor)
   ss2 = ss;
   EXPECT_EQ(ss.getId(), 0);
   EXPECT_EQ(ss2.getId(), 1);
+  Spreadsheet ss3(ss);
+  EXPECT_EQ(ss3.getId(), 2);
+  EXPECT_EQ(ss3.getCellAt(3, 3).getValue(), 0);
+  SpreadsheetCell ssc = SpreadsheetCell(8);
+  ss2.setCellAt(0, 0, ssc);
+  EXPECT_EQ(ss2.getCellAt(0, 0).getValue(), 8);
+  EXPECT_EQ(ss.getCellAt(0, 0).getValue(), 0);
 }
